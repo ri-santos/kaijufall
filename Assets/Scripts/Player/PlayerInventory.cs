5f6c28e043769df0cd81065c1d68930337ccfcca
Miguel Miranda <160755200+Miguel-Miranda019@users.noreply.github.com>
2025-06-16 14:34:29 +0100

    DONE

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using Mono.Cecil.Cil;
+//using Mono.Cecil.Cil;
 
 public class PlayerInventory : MonoBehaviour
 {
